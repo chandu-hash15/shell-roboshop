d0629579bb64f1f34b52cb4465b454500c6ab855
Chandu <chandutharun42@gmail.com>
2025-10-07 21:09:19 +0530

    creating instances through script  and updating route 53 records 6

--- roboshop.sh
+++ roboshop.sh
@@ -4,23 +4,22 @@ AMI_ID="ami-09c813fb71547fc4f"
 SG_ID="sg-0a2728062dcacf005"
 ZONE_ID="Z00484781U0O1QG6VX0D8"
 Domain_Name="mitha.fun"
-for instance in "$@"
+for instance in $@ # mongodb redis mysql
 do
-        INSTANCE_ID=$(aws ec2 run-instances --image-id "$AMI_ID" --instance-type t2.micro --security-group-ids "$SG_ID" --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
 
-        # Use string comparison and quote the variable. If instance is not 'frontend' use private IP.
-        if [ "$instance" != "frontend" ]; then
-                IP=$(aws ec2 describe-instances --instance-ids "$INSTANCE_ID" --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
-                # Replace spaces in instance name with hyphens for a valid DNS record name
-                SANITIZED_NAME="${instance// /-}"
-                RECORD_NAME="$SANITIZED_NAME.$Domain_Name"
-        else
-                IP=$(aws ec2 describe-instances --instance-ids "$INSTANCE_ID" --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
-                RECORD_NAME="$Domain_Name"
-        fi
- echo "$instance : $IP"
+    # Get Private IP
+    if [ $instance != "frontend" ]; then
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+        RECORD_NAME="$instance.$Domain_Name" # mongodb.daws86s.fun
+    else
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+        RECORD_NAME="$Domain_Name" # daws86s.fun
+    fi
 
-aws route53 change-resource-record-sets \
+    echo "$instance: $IP"
+
+    aws route53 change-resource-record-sets \
     --hosted-zone-id $ZONE_ID \
     --change-batch '
     {
@@ -39,3 +38,5 @@ aws route53 change-resource-record-sets \
     }
     '
 done
+    '
+d
