e89c929a33208d60d30b19c771a9ac483b8b88d0
Chandu <chandutharun42@gmail.com>
2025-10-07 21:04:15 +0530

    creating instances through script  and updating route 53 records 5

--- roboshop.sh
+++ roboshop.sh
@@ -4,18 +4,20 @@ AMI_ID="ami-09c813fb71547fc4f"
 SG_ID="sg-0a2728062dcacf005"
 ZONE_ID="Z00484781U0O1QG6VX0D8"
 Domain_Name="mitha.fun"
-for instance in $@
+for instance in "$@"
 do
-INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
+        INSTANCE_ID=$(aws ec2 run-instances --image-id "$AMI_ID" --instance-type t2.micro --security-group-ids "$SG_ID" --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
 
- if [ $instance -ne "frontend" ]; then
-   IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
-   RECORD_NAME="$instance.$Domain_Name"
-
- else
-   IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
-    RECORD_NAME="$Domain_Name" 
- fi
+        # Use string comparison and quote the variable. If instance is not 'frontend' use private IP.
+        if [ "$instance" != "frontend" ]; then
+                IP=$(aws ec2 describe-instances --instance-ids "$INSTANCE_ID" --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+                # Replace spaces in instance name with hyphens for a valid DNS record name
+                SANITIZED_NAME="${instance// /-}"
+                RECORD_NAME="$SANITIZED_NAME.$Domain_Name"
+        else
+                IP=$(aws ec2 describe-instances --instance-ids "$INSTANCE_ID" --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+                RECORD_NAME="$Domain_Name"
+        fi
  echo "$instance : $IP"
 
 aws route53 change-resource-record-sets \
