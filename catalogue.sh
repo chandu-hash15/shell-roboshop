193f93e1a1cb8c73459c70ab9077131858af4a5a
Chandu <chandutharun42@gmail.com>
2025-10-08 16:42:35 +0530

    catalogue service 11

--- catalogue.sh
+++ catalogue.sh
@@ -1,90 +1,88 @@
 #!/bin/bash
 
+USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-LOG_FOLDER="/var/log/sheel_catalogue"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
-mkdir -p "$LOG_FOLDER"
-SCRIPT_DIR=$(pwd)
+
+LOGS_FOLDER="/var/log/shell-roboshop"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+SCRIPT_DIR=$PWD
 MONGODB_HOST=mongodb.mitha.fun
-USER_ID=$(id -u)
-if [ $USER_ID -ne 0 ]; then
-    echo -e "you are not a roor user $R failed $N"
-    exit 1
-else
-    echo -e "you are root user $G success $N"
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
+
+mkdir -p $LOGS_FOLDER
+echo "Script started executed at: $(date)" | tee -a $LOG_FILE
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: Please run this script with root privelege"
+    exit 1 # failure is other than 0
 fi
 
-validate() {
- if [ $1 -ne 0 ];then
-     echo -e "$2 ------- $R failed $N"
-     exit 1
- else
-    echo -e "$2 ------- $G success $N"
- fi 
+VALIDATE(){ # functions receive inputs through args just like shell script args
+    if [ $1 -ne 0 ]; then
+        echo -e "$2 ... $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N" | tee -a $LOG_FILE
+    fi
 }
 
-##### Nodejs installation #####
+##### NodeJS ####
 dnf module disable nodejs -y &>>$LOG_FILE
-validate $? "disabling nodejs"
-
-dnf module enable nodejs:20 -y &>>$LOG_FILE
-validate $? "enabling nodejs"
-
+VALIDATE $? "Disabling NodeJS"
+dnf module enable nodejs:20 -y  &>>$LOG_FILE
+VALIDATE $? "Enabling NodeJS 20"
 dnf install nodejs -y &>>$LOG_FILE
-validate $? "nodejs"
+VALIDATE $? "Installing NodeJS"
 
 id roboshop &>>$LOG_FILE
 if [ $? -ne 0 ]; then
-  useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
-  validate $? "adding roboshop user"
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
+    VALIDATE $? "Creating system user"
 else
-  echo -e "roboshop user already exists ------- $G success $N"
+    echo -e "User already exist ... $Y SKIPPING $N"
 fi
 
-mkdir -p /app 
-validate $? "creating app directory" 
+mkdir -p /app
+VALIDATE $? "Creating app directory"
 
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>>$LOG_FILE
-validate $? "downloading catalogue component"
+VALIDATE $? "Downloading catalogue application"
 
-cd /app
-validate $? "changing directory to /app"
+cd /app 
+VALIDATE $? "Changing to app directory"
 
-rm -rf  /app/*
-validate $? "removing existing code"
+rm -rf /app/*
+VALIDATE $? "Removing existing code"
 
-unzip /tmp/catalogue.zip
-validate $? "unzipping catalogue component"
+unzip /tmp/catalogue.zip &>>$LOG_FILE
+VALIDATE $? "unzip catalogue"
 
 npm install &>>$LOG_FILE
-validate $? "installing nodejs dependencies"
+VALIDATE $? "Install dependencies"
 
 cp $SCRIPT_DIR/catalogue.service /etc/systemd/system/catalogue.service
-validate $? "copying catalogue service file"
+VALIDATE $? "Copy systemctl service"
 
-
-systemctl daemon-reload &>>$LOG_FILE
-validate $? "reloading systemd"
-
+systemctl daemon-reload
 systemctl enable catalogue &>>$LOG_FILE
-validate $? "enabling catalogue service"
-
-systemctl start catalogue &>>$LOG_FILE
-validate $? "starting catalogue service"
-
+VALIDATE $? "Enable catalogue"
 
 cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
-validate $? "copying mongo.repo file"   
+VALIDATE $? "Copy mongo repo"
 
 dnf install mongodb-mongosh -y &>>$LOG_FILE
-validate $? "mongodb-mongosh"
+VALIDATE $? "Install MongoDB client"
 
-mongosh --host $MONGODB_HOST </app/db/master-data.js
-validate $? "loading catalogue schema"
+INDEX=$(mongosh mongodb.daws86s.fun --quiet --eval "db.getMongo().getDBNames().indexOf('catalogue')")
+if [ $INDEX -le 0 ]; then
+    mongosh --host $MONGODB_HOST </app/db/master-data.js &>>$LOG_FILE
+    VALIDATE $? "Load catalogue products"
+else
+    echo -e "Catalogue products already loaded ... $Y SKIPPING $N"
+fi
 
-systemctl restart catalogue &>>$LOG_FILE
-validate $? "restarting catalogue service"
+systemctl restart catalogue
+VALIDATE $? "Restarted catalogue"
