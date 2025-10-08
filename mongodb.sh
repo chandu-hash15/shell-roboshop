77b1e0181fe03d6ab5eee2231a2b73cf25b7239c
Chandu <chandutharun42@gmail.com>
2025-10-08 06:19:21 +0530

    creating  mongodb 1

--- mongodb.sh
+++ mongodb.sh
@@ -19,12 +19,14 @@ else
 fi
 
 validate() {
-    if [ $1 -ne 0 ],then
-        echo -e "$2 ----- failed $R failed $N"
-        exit 1
-    esle
-        echo -e " $2 ----- $G success $N"
-    fi
+if [ $1 -ne 0 ],then
+    echo -e "$2 ----- failed $R failed $N"
+    exit 1
+
+else  
+   echo -e " $2 ----- $G success $N"
+fi
+
 }
 
 
